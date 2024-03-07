3335240aec7519004a0c19be61049199775caed8
jluna28 <little.luna02@gmail.com>
2024-03-06 16:59:08 -0800

    read in airlines and airports

--- src/src/main/scala/Main.scala
+++ src/src/main/scala/Main.scala
@@ -1,12 +1,16 @@
 import org.apache.spark.SparkContext._
 import scala.io._
-import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import shapeless.syntax.std.tuple.productTupleOps
 import scala.collection._
 
 object Main {
+  case class Airline(code: String, name: String)
+  case class Airport(code: String, name: String, city: String, state: String)
+
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
@@ -14,21 +18,16 @@ object Main {
     val conf = new SparkConf().setAppName("Main").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
-    val airports = Source.fromFile("airports.csv")
-      .getLines
-      .toList
+    val airports = sc.textFile("src/airports.csv")
       .map(_.split(",").map(_.trim))
       .map(arr => Airport(arr(0), arr(1), arr(2), arr(3)))
 
-    val airlines = Source.fromFile("airlines.csv")
-      .getLines
-      .toList
+    val airlines = sc.textFile("src/airlines.csv")
       .map(_.split(",").map(_.trim))
       .map(arr => Airline(arr(0), arr(1)))
 
-    airlines.foreach(airline => println(airline.mkString(", ")))
-
-    case class Airline(code: String, name: String)
-    case class Airport(code: String, name: String, city: String, state: String)
+    //airlines.foreach(airline => println(airline.mkString("", ", ", "")))
+    //airports.foreach(airport => println(airport.mkString("", ", ", "")))
   }
+}
 
