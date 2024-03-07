e7a33343e9cda8e7fdac7649e8b2d52d78e42f9b
jluna28 <little.luna02@gmail.com>
2024-03-06 16:18:13 -0800

    reading in data

--- src/src/main/scala/Main.scala
+++ src/src/main/scala/Main.scala
@@ -11,9 +11,24 @@ object Main {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
-    val conf = new SparkConf().setAppName("NameOfApp")
+    val conf = new SparkConf().setAppName("Main").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
+    val airports = Source.fromFile("airports.csv")
+      .getLines
+      .toList
+      .map(_.split(",").map(_.trim))
+      .map(arr => Airport(arr(0), arr(1), arr(2), arr(3)))
 
+    val airlines = Source.fromFile("airlines.csv")
+      .getLines
+      .toList
+      .map(_.split(",").map(_.trim))
+      .map(arr => Airline(arr(0), arr(1)))
+
+    airlines.foreach(airline => println(airline.mkString(", ")))
+
+    case class Airline(code: String, name: String)
+    case class Airport(code: String, name: String, city: String, state: String)
   }
 
