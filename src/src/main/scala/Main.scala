88b5294b4e595beab781841631ba947624dbee93
jluna28 <little.luna02@gmail.com>
2024-03-06 17:44:26 -0800

    add flights object -- NEED TO LOCALLY PUT FLIGHTS.CSV IN SRC

--- src/src/main/scala/Main.scala
+++ src/src/main/scala/Main.scala
@@ -10,6 +10,8 @@ import scala.collection._
 object Main {
   case class Airline(code: String, name: String)
   case class Airport(code: String, name: String, city: String, state: String)
+  case class Flight(year: Int, month: Int, day: Int, day_of_week: Int, airline: String, flight_num: Int,
+                    origin: String, dest: String, scheduled_depart: Int, actual_depart: Int, delay: Int)
 
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
@@ -26,8 +28,16 @@ object Main {
       .map(_.split(",").map(_.trim))
       .map(arr => Airline(arr(0), arr(1)))
 
+    val flights = sc.textFile("src/flights.csv")
+      .map(_.split(",").map(_.trim))
+      .filter(_(0) != "YEAR")
+      .filter(arr => !arr.exists(_.isEmpty))
+      .map(arr => Flight(arr(0).toInt, arr(1).toInt, arr(2).toInt, arr(3).toInt, arr(4), arr(5).toInt,
+        arr(7), arr(8), arr(9).toInt, arr(10).toInt, arr(11).toInt))
+
     //airlines.foreach(airline => println(airline.mkString("", ", ", "")))
     //airports.foreach(airport => println(airport.mkString("", ", ", "")))
+    //flights.foreach(flight => println(flight.mkString("", ", ", "")))
   }
 }
 
